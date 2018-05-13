c025db24eed4633f7c232e8a89da67f4b3855acb
Daniel Aparisi <danielapa_ellider@hotmail.com>
2018-05-13 17:38:39 +0200

    Fixed menu and added backgrounds
    Fixed and cleaned the menu of sceneObjects
    Added adaptation to screen sizes for the menu of SceneObjects
    Added an sticky footer
    Added options for backgrounds

--- Web/js/funct.js
+++ Web/js/funct.js
@@ -1,31 +1,60 @@
-jQuery( document ).ready(function($) {
-    $('[data-toggle="tooltip"]').tooltip();
-    $('body').on('click', '#objectsVar button.sceneObjects', function(e){
-      e.preventDefault();
-      var $this = $(this);
-      if ($this.hasClass('active')) {
-        $this.removeClass('active');
-        // quitar objecto de escena
-      } else {
-        $this.addClass('active');
-        // poner objecto de escena
-      }
-    })
-    $('body').on('click', '#toggleVar button#toggleObjectVar', function(e){
-      e.preventDefault();
-      var $this = $(this);
-      var $listObjects = $( "#listObjects" );
-      if ($listObjects.hasClass('shown')) {
-        $this.removeClass('up').addClass('down');
-        $this.children('i').removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
-        $listObjects.removeClass('shown').addClass('hidden');
+var timeAnimation = 1000;
 
-        $('#listObjects').hide('slow');
-      } else {
-        $this.removeClass('down').addClass('up');
-        $this.children('i').removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
-        $listObjects.removeClass('hidden').addClass('shown');
-        $('#listObjects').show('slow');
-      }
-    })
+jQuery( document ).ready(function($) {
+  // For tooltips in body
+  $("body").tooltip({
+      selector: '[data-toggle="tooltip"]',
+      container: 'body',
+      placement: 'auto',
+      html: true,
+      trigger: 'hover'
+  });
+
+  // Event when click on a sceneObject
+  $('body').on('click', '#objectsVar button.sceneObjects', function(e){
+    e.preventDefault();
+    var $this = $(this);
+    if ($this.hasClass('active')) {
+      $this.removeClass('active');
+      // quitar objecto de escena
+    } else {
+      $this.addClass('active');
+      // poner objecto de escena
+    }
+  });
+
+  // Event when click on the toggleObjectVar
+  $('body').on('click', '#toggleVar button#toggleObjectVar', function(e){
+    e.preventDefault();
+    var $this = $(this);
+    var $objectsVar = $("#objectsVar");
+    if ($this.hasClass('shown')) {
+      $this.removeClass('shown').addClass('hidden');
+      $this.children('i').removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
+      $objectsVar.animate({
+        height: "toggle"
+      }, timeAnimation);
+    } else {
+      $this.removeClass('hidden').addClass('shown');
+      $this.children('i').removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
+      $objectsVar.animate({
+        height: "toggle"
+      }, timeAnimation);
+    }
+  });
+
+  $('body').on('click', '.setBg:not(.active)', function(e){
+    e.preventDefault();
+    var $this = $(this);
+    var img = $this.data('img');
+    var $imgBg = $('#imgBg');
+    if (img == 'none') {
+      $imgBg.fadeOut('slow');
+    } else {
+      $imgBg.attr('src', 'sceneBackgrounds/' + img);
+      $imgBg.fadeIn('slow');
+    }
+    $('.setBg').removeClass('active');
+    $this.addClass('active');
+  });
 });
