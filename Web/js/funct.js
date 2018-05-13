571fa0205f20a2d82a436a031f42e072a72ec499
Alexis Ruiz <alexis.ruiz01@estudiant.upf.edu>
2018-05-13 10:29:06 +0200

    Solution on buttons dropdown problem

--- Web/js/funct.js
+++ Web/js/funct.js
@@ -11,16 +11,20 @@ jQuery( document ).ready(function($) {
         // poner objecto de escena
       }
     })
-    $('body').on('click', '#objectsVar button#toggleObjectVar', function(e){
+    $('body').on('click', '#toggleVar button#toggleObjectVar', function(e){
       e.preventDefault();
       var $this = $(this);
-      if ($this.hasClass('shown')) {
-        $this.removeClass('shown').addClass('hidden');
+      var $listObjects = $( "#listObjects" );
+      if ($listObjects.hasClass('shown')) {
+        $this.removeClass('up').addClass('down');
         $this.children('i').removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
+        $listObjects.removeClass('shown').addClass('hidden');
+
         $('#listObjects').hide('slow');
       } else {
-        $this.removeClass('hidden').addClass('shown');
+        $this.removeClass('down').addClass('up');
         $this.children('i').removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
+        $listObjects.removeClass('hidden').addClass('shown');
         $('#listObjects').show('slow');
       }
     })
