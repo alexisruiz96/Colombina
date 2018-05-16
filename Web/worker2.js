868e38903a39d28e1ddea73c6c177cb9be4ae18c
Alexis Ruiz <alexis.ruiz01@estudiant.upf.edu>
2018-05-16 16:49:23 +0200

    Integration app on web

---
+++ Web/worker2.js
@@ -0,0 +1,49 @@
+let ptr;
+let cvbridge;
+
+function initMemory(imageData) {
+
+	cvbridge = new Module.CVBridge(imageData.width,imageData.height);
+	ptr      = Module._malloc(imageData.width * imageData.height * 4);
+
+}
+
+function faceDetect(imageData) {
+
+	let facialPointsArray = [];
+
+	if (ptr == undefined) {
+		initMemory(imageData);
+	}
+
+	//let ptr= Module._malloc(imageData.width * imageData.height * 4);
+
+	Module.HEAPU8.set(imageData.data,ptr);
+	let facialPoints = cvbridge.processFrame(ptr);
+  //cvbridge.delete();
+
+  for (let i = 0; i < facialPoints.size(); i++) {
+    facialPointsArray.push({
+      x: facialPoints.get(i)[0],
+      y: facialPoints.get(i)[1]
+    });
+  }
+
+  //Module._free(ptr);
+
+	postMessage({ features: facialPointsArray });
+}
+
+self.onmessage = function (e) {
+	switch (e.data.cmd) {
+		case 'faceDetect':
+			faceDetect(e.data.img);
+			break;
+	}
+}
+
+self.onerror = function (e) {
+	console.log(e);
+}
+debugger
+console.log('done loading worker')
